ac032c92cc2fb651e2378778a8aafee1c552bf4b
Erdef <114429829+Erdef119@users.noreply.github.com>
2024-06-22 17:29:59 +0200

    First push

---
+++ stringobfuscator/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace stringobfuscate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ObfuscateButton = new System.Windows.Forms.Button();
+            this.DeobfuscateButton = new System.Windows.Forms.Button();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ObfuscateButton
+            // 
+            this.ObfuscateButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ObfuscateButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ObfuscateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ObfuscateButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.ObfuscateButton.Location = new System.Drawing.Point(12, 287);
+            this.ObfuscateButton.Name = "ObfuscateButton";
+            this.ObfuscateButton.Size = new System.Drawing.Size(518, 95);
+            this.ObfuscateButton.TabIndex = 0;
+            this.ObfuscateButton.Text = "Obfuscate!";
+            this.ObfuscateButton.UseVisualStyleBackColor = false;
+            this.ObfuscateButton.Click += new System.EventHandler(this.ObfuscateButton_Click_1);
+            // 
+            // DeobfuscateButton
+            // 
+            this.DeobfuscateButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.DeobfuscateButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.DeobfuscateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DeobfuscateButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.DeobfuscateButton.Location = new System.Drawing.Point(541, 287);
+            this.DeobfuscateButton.Name = "DeobfuscateButton";
+            this.DeobfuscateButton.Size = new System.Drawing.Size(518, 95);
+            this.DeobfuscateButton.TabIndex = 1;
+            this.DeobfuscateButton.Text = "Deobfuscate!";
+            this.DeobfuscateButton.UseVisualStyleBackColor = false;
+            this.DeobfuscateButton.Click += new System.EventHandler(this.DeobfuscateButton_Click_1);
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.InputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.InputTextBox.Location = new System.Drawing.Point(12, 37);
+            this.InputTextBox.Multiline = true;
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(517, 237);
+            this.InputTextBox.TabIndex = 2;
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.OutputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.OutputTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.OutputTextBox.Location = new System.Drawing.Point(542, 37);
+            this.OutputTextBox.Multiline = true;
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(517, 237);
+            this.OutputTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "StringObfuscate";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1040, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(19, 21);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "x";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(269, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(517, 13);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(175, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Encryption string:";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1071, 398);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.DeobfuscateButton);
+            this.Controls.Add(this.ObfuscateButton);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "String Obfuscator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ObfuscateButton;
+        private System.Windows.Forms.Button DeobfuscateButton;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.TextBox OutputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ stringobfuscator/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stringobfuscate
+{
+    public partial class Form1 : Form
+    {
+        private string key = "changeme";
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = key;
+        }
+
+        private string ObfuscateString(string input)
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < inputBytes.Length; i++)
+            {
+                sb.Append((char)(inputBytes[i] ^ keyBytes[i % keyBytes.Length]));
+            }
+
+            return sb.ToString();
+        }
+
+        private string DeobfuscateString(string input)
+        {
+            return ObfuscateString(input);
+        }
+
+        private void ObfuscateButton_Click(object sender, EventArgs e)
+        {
+            string input = InputTextBox.Text;
+            string obfuscated = ObfuscateString(input);
+            OutputTextBox.Text = obfuscated;
+        }
+
+        private void DeobfuscateButton_Click(object sender, EventArgs e)
+        {
+            string input = InputTextBox.Text;
+            string deobfuscated = DeobfuscateString(input);
+            OutputTextBox.Text = deobfuscated;
+        }
+
+        private void ObfuscateButton_Click_1(object sender, EventArgs e)
+        {
+            string input = InputTextBox.Text;
+            string obfuscated = ObfuscateString(input);
+            OutputTextBox.Text = obfuscated;
+        }
+
+        private void DeobfuscateButton_Click_1(object sender, EventArgs e)
+        {
+            string input = OutputTextBox.Text;
+            string deobfuscated = DeobfuscateString(input);
+            InputTextBox.Text = deobfuscated;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            key = textBox1.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
